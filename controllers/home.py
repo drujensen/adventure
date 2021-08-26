9781fa73200e5a323d029d68e65a83b920438038
Dru Jensen <drujensen@gmail.com>
2021-08-26 09:45:39 -0700

    replace Jinja2 with Mako

--- controllers/home.py
+++ controllers/home.py
@@ -2,13 +2,13 @@ from fastapi import APIRouter
 from fastapi import Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-
+from mako.template import Template
+from mako.lookup import TemplateLookup
 
 home_router = APIRouter()
-views = Jinja2Templates(directory="views")
-
+views = TemplateLookup(directories=['views'])
 
 @home_router.get("/")
-async def home(request: Request):
-	return views.TemplateResponse("home/index.html",{"request":request})
-
+def home(request: Request):
+	template = views.get_template("/home/index.html")
+	return HTMLResponse(template.render())
